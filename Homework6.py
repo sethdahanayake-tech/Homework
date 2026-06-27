21b549553e67b74b35e37f4a34277a397fede875
sethdahanayake-tech <seth.dahanayake@gmail.com>
2026-06-28 10:28:14 +1200

    Add files via upload

---
+++ Homework6.py
@@ -0,0 +1,10 @@
+# Take input from the user
+char = input("Enter a character: ")
+
+# Check if the character is an alphabet letter
+if char.isalpha():
+    print(f"'{char}' is an alphabet.")
+else:
+    print(f"'{char}' is not an alphabet.")
+
+    
